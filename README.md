d27e38302ef2462a31bb94d58e1a503069a3af6f
devcarolinealmeida <carolineitalia85@gmail.com>
2024-01-21 15:29:15 +0100

    add First version: 'User vs computer'

--- README.md
+++ README.md
@@ -14,9 +14,16 @@ Acceptance Criteria:
 -- rock crushes scissors;
 -- paper covers rock;
 -- scissors cuts paper;
--- three possible outcomes: a draw, a win or a loss;
+-- three possible outcomes: a tie, a win or a loss;
 -- If both players choose the same shape, the game is tied and is usually replayed to break the tie;
 
 
+/*--- To do ---*/
 
-
+/* First version: 'User vs computer' */
+1. Create a list of the weapons for the computer;
+2. Function to raffle a random weapon;
+3. 3 Buttons for the User choose a weapon;
+4. When the user clicks on the button weapon, the computer choose a random weapon;
+5. Create a function to compairs who wins;
+6. Show in the screen the results;
--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+const weaponsComputer = ['rock','paper','scissors'];
+
+console.log(weaponsComputer);
+
+const playGame = (weaponUser) => {
+    raffleWeapon();
+    console.log(weaponUser, randomWeapon);
+    result = '';
+
+    if(weaponUser === randomWeapon) {
+        result = 'Its a tie';
+    } 
+    else {
+        switch(weaponUser) {
+            case 'rock':
+                result = (randomWeapon === 'scissors' ? 'You win' : 'You lose');
+                break;
+            case 'paper':
+                result = (randomWeapon === 'rock' ? 'You win' : 'You lose');
+                break;
+            case 'scissors' :
+                result = (randomWeapon == 'paper' ? 'You win' : 'You lose');
+        }
+    }
+    console.log(result);
+}
+
+
+const raffleWeapon = () => {
+    let i = Math.floor(Math.random() * weaponsComputer.length);
+    randomWeapon = weaponsComputer[i];
+    /* console.log(randomWeapon); */
+}
 
